93f6bced106dfe31e6786514fb0c20b71840ca4a
IanRichter <ianrichter123@gmail.com>
2019-09-14 14:20:55 +0200

    Add spawnin heuristic classes

--- VR Cargo Ship/Assets/Scripts/LaneManagement/SpawnLane.cs
+++ VR Cargo Ship/Assets/Scripts/LaneManagement/SpawnLane.cs
@@ -1,7 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SpawnLane : MonoBehaviour {
-    
+
+	public Zone leftZone;
+	public Zone rightZone;
+
+	private GameObject activeObject;
+	public GameObject ActiveObject {
+		get {
+			return activeObject;
+		}
+	}
+	
+	private EntitySpawnConfig entityConfig;
+	public EntitySpawnConfig EntityConfig {
+		get {
+			return entityConfig;
+		}
+	}
+
+
+	public bool IsEmpty {
+		get {
+			return !activeObject;
+		}
+	}
+
+	public void SpawnEntity(EntitySpawnConfig entityConfig, EntityMoveDirection direction) {
+
+	}
+
 }
