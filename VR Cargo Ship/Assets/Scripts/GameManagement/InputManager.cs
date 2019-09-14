01955adfa78ba5b1781b47e7c288f8fe87341c2d
IanRichter <ianrichter123@gmail.com>
2019-09-14 21:39:04 +0200

    Add HTC Unity Plugin

--- VR Cargo Ship/Assets/Scripts/GameManagement/InputManager.cs
+++ VR Cargo Ship/Assets/Scripts/GameManagement/InputManager.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+using HTC.UnityPlugin.Vive;
 
 public class InputManager : MonoBehaviour {
 
+	public Hand leftHand;
+	public Hand rightHand;
+
 	public bool IsLeft {
 		get {
-			return Input.GetKey(KeyCode.J);
+			return Input.GetKey(KeyCode.J) || ViveInput.GetPress(HandRole.LeftHand, ControllerButton.Trigger);
 		}
 	}
 
 	public bool IsRight {
 		get {
-			return Input.GetKey(KeyCode.K);
+			return Input.GetKey(KeyCode.K) || ViveInput.GetPress(HandRole.RightHand, ControllerButton.Trigger);
 		}
 	}
 
