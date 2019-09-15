7baef040ca2e0818bf6bc8b43bf1cc461c8d85c1
IanRichter <ianrichter123@gmail.com>
2019-09-15 13:43:19 +0200

    Fix button

--- VR Cargo Ship/Assets/Scripts/UI/ConfirmMenuButton.cs
+++ VR Cargo Ship/Assets/Scripts/UI/ConfirmMenuButton.cs
@@ -5,16 +5,7 @@ public class ConfirmMenuButton : MonoBehaviour {
 	public delegate void ConfirmAction();
 	public ConfirmAction OnClick;
 
-
 	private void OnTriggerEnter(Collider other) {
-		if (!other.gameObject.GetComponent<ControllerClickBall>()) {
-			return;
-		}
-
-		// TODO: Play Sound
-	}
-
-	private void OnTriggerExit(Collider other) {
 		if (!other.gameObject.GetComponent<ControllerClickBall>()) {
 			return;
 		}
